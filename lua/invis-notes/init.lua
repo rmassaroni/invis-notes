1a9c792f918a2eb73feda9467dc939ee9ea19d02
rjm <robjmass@gmail.com>
2024-03-15 15:01:41 -0400

    create_txt_file()

--- lua/invis-notes/init.lua
+++ lua/invis-notes/init.lua
@@ -1,5 +1,16 @@
 local M = {}
 
+function M.create_txt_file(file_name, content)
+    local file = io.open(file_name, "w")
+    if file then
+        file:write(content)
+        file:close()
+        print("File created successfully: " .. file_name)
+    else
+        print("Error: Unable to create the file.")
+    end
+end
 
+M.create_txt_file("example.txt", "Hello, this is a Lua-generated text file!\nYou can write anything you want here.\n")
 
 return M
---
+++ plugin/invis-notes.vim
@@ -0,0 +1,1 @@
+command! -nargs=0 CompleteTodo lua require("invis-notes").create_txt_file()
